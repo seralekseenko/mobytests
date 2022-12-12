19d0a33f547c258d50454d82531161af47b58cde
seralekseenko <seralekseenko@ukr.net>
2022-12-12 20:34:18 +0200

    create: pom.xml, Finder.java

---
+++ src/main/java/seralekseenko/experiments/Finder.java
@@ -0,0 +1,26 @@
+package seralekseenko.experiments;
+
+import java.util.OptionalInt;
+
+
+public class Finder {
+  
+  public static OptionalInt findIndex(int[] sortedArray, target) {
+
+    for (var index = 0; index < sortedArray.length; index++) {
+      if (target == sortedArray[index]) {
+        return t
+      }
+
+    }
+
+
+
+        return new OptionalInt().empty();
+  
+
+  }
+
+
+
+}
