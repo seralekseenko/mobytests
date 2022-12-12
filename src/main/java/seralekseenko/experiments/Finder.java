e56434baa041a9198442a60f5430eb3661cf62b6
seralekseenko <seralekseenko@ukr.net>
2022-12-12 21:19:55 +0200

    made tests

--- src/main/java/seralekseenko/experiments/Finder.java
+++ src/main/java/seralekseenko/experiments/Finder.java
@@ -4,23 +4,19 @@ import java.util.OptionalInt;
 
 
 public class Finder {
-  
-  public static OptionalInt findIndex(int[] sortedArray, target) {
+
+  public static OptionalInt findIndex(int[] sortedArray, int target) {
 
     for (var index = 0; index < sortedArray.length; index++) {
       if (target == sortedArray[index]) {
-        return t
+        return OptionalInt.of(index + 1);
       }
-
     }
 
+    return OptionalInt.empty();
 
-
-        return new OptionalInt().empty();
-  
 
   }
-
 
 
 }
---
+++ src/test/java/seralekseenko/experiments/FinderTest.java
@@ -0,0 +1,38 @@
+package seralekseenko.experiments;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+class FinderTest {
+
+  private final Random rnd = ThreadLocalRandom.current();
+
+  @RepeatedTest(1)
+  @DisplayName("findIndex' method with typical arguments ...")
+  public void findIndex() {
+    //preparing data
+    var uniqueSortedInts = rnd.ints(-50, 50)
+        .distinct() // for unique numbers
+        .limit(100) // breaking infinity cycle
+        .sorted()
+        .toArray();
+    var expectedIndex = rnd.nextInt(0, 100);
+    var target = uniqueSortedInts[expectedIndex];
+    var message = "input array: %s%ninput target: %d".formatted(Arrays.toString(uniqueSortedInts),
+        target);
+    // run the action
+    var result = assertDoesNotThrow(() -> Finder.findIndex(uniqueSortedInts, target).getAsInt(),
+        "Unexpected exception with valid arguments!");
+    // check the result
+    assertWithMessage(message)
+        .that(result)
+        .isEqualTo(expectedIndex);
+
+  }
+}
